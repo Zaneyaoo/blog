bb71aa62ccd32ea75994a0d23ac95009d8959b1f
Zane <123451374+Zaneyaoo@users.noreply.github.com>
2024-11-14 03:16:11 +0800

    vercel undo

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HUGO_VERSION="0.138.0"
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-./hugo --gc --minify
